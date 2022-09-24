e163b8f87d03c67b253c56f808a5942625f3fbd3
Vinayak Bector <69417893+VinayakBector2002@users.noreply.github.com>
2022-09-24 12:16:13 -0400

    Project added

---
+++ PracticeWidget/PracticeWidget/PracticeWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeWidgetApp.swift
+//  PracticeWidget
+//
+//  Created by Vinayak Bector on 2022-09-24.
+//
+
+import SwiftUI
+
+@main
+struct PracticeWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
